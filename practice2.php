c777e78f22e5cfaf9144d09b23beea7a9bdc0aa1
Hayato-Nakagawa <nono.saya-5533_r@kha.biglobe.ne.jp>
2019-11-09 03:09:35 +0000

    PHP-Laravel02課題

---
+++ practice2.php
@@ -0,0 +1,16 @@
+<?php
+$integer = 20;
+$string = "abc";
+$new_integer = $integer + 10;
+$red_fruit = "APPLE";
+$yellow_fruit = 'Banana';
+$array = [2017, 2018, 2019, 2020];
+$array2 = ["spring", "summer", "autumn", "winter"];
+$animals = [
+  "cat" => "猫",
+  "dog" => "犬",
+  "bird" => "鳥"
+];
+$result = true;
+$value = "AAA";
+echo $value . "BBB";
